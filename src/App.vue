8d2d7217fc0ba32d1b4de7eb6bf446b8e0ffea52
yang_zhuo <2431542525@qq.com>
2023-09-18 23:10:08 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<script setup>
+import { marked } from 'marked'
+import { ref } from 'vue'
+
+const showWriteBox = ref(true)
+const showPreviewBox = ref(false)
+
+function toggleWriteBox(){
+    showWriteBox.value = true
+    showPreviewBox.value = false
+}
+
+function togglePreviewBox(){
+    showWriteBox.value = false
+    showPreviewBox.value = true
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="header">
+            <div class="ll-box"></div>
+            <div class="left-box" :class="{ 'selected-box':showWriteBox,'unselected-box':showPreviewBox}">
+                <button class="left-button" @click="toggleWriteBox" :class="{ 'selected-font': showPreviewBox }">write</button>
+            </div>
+            <div class="right-box" :class="{ 'selected-box':showPreviewBox,'unselected-box':showWriteBox }">
+                <button class="right-button" @click="togglePreviewBox" :class="{ 'selected-font': showWriteBox }">preview</button>
+            </div>
+            <div class="rr-box"></div>
+        </div>
+
+        <div class="main">
+            <textarea  v-if="showWriteBox" placeholder="Leave a comment" class="google-font write-box" cols="90" rows="20">write-box</textarea>
+            <div  v-if="showPreviewBox" class="preview-box">preview-box</div>
+        </div>
+
+        <div class="footer google-font">Remember, contributions to this repository should follow our <a href="#">Github
+                Community Guidelines.</a></div>
+    </div>
+</template>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: space-between;
+    background-color: #ffffff;
+    border: 1px solid #d0d7de;
+    border-radius: 6px;
+    padding: 1rem;
+    margin: 1rem;
+    /* gap: 1rem; */
+}
+
+.header {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    border-bottom: 1px solid #d0d7de;
+    padding-bottom: 1rem;
+}
+
+.left-box{
+    flex: 20%;
+    background-color: #ffffff;
+}
+
+.right-box{
+    flex: 20%;
+    background-color: #ffffff;
+}
+
+.selected-font{
+    color: #999999;
+}
+
+.selected-box{
+    border: 1px solid #d0d7de;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    border-bottom: none;
+}
+
+.unselected-box{
+    border-bottom: 1px solid #d0d7de;
+}
+
+.preview-box{
+    width: 500px;
+    height: 200px;
+    background-color: #fff;
+}
+
+.ll-box{
+    flex:2%;
+    border-bottom: 1px solid #d0d7de;
+}
+.rr-box {
+    flex: 60%;
+    border-bottom: 1px solid #d0d7de;
+}
+
+.footer {
+    font-size: small;
+}
+
+.google-font {
+    font-family: Roboto, sans-serif;
+}
+
+button{
+    border:none;
+    background-color: transparent;
+    outline: none;
+}
+
+button:hover{
+    color: black;
+}
+</style>
---
+++ src/__VLS_template.js
@@ -0,0 +1,129 @@
+import { __VLS_publicComponent, __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from './App.vue';
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_publicComponent, new () => {}>> & InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & typeof __VLS_publicComponent & (new () => { $slots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {} &
+{ 'container'?: boolean; } &
+{ 'header'?: boolean; } &
+{ 'left'?: boolean; } &
+{ 'right'?: boolean; } &
+{ 'right'?: boolean; } &
+{ 'main'?: boolean; } &
+{ 'footer'?: boolean; } &
+{ 'google-font'?: boolean; };
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {};
+({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).button; ({} as __VLS_IntrinsicElements).button; ({} as __VLS_IntrinsicElements).button; ({} as __VLS_IntrinsicElements).button;
+({} as __VLS_IntrinsicElements).textarea; ({} as __VLS_IntrinsicElements).textarea;
+({} as __VLS_IntrinsicElements).a; ({} as __VLS_IntrinsicElements).a;
+{
+const __VLS_0 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_1 = __VLS_asFunctionalComponent(__VLS_0, {});
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_2 = __VLS_1({ ...{ ...{}, class: ("container"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+{
+const __VLS_4 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_5 = __VLS_asFunctionalComponent(__VLS_4, {});
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_6 = __VLS_5({ ...{ ...{}, class: ("header"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_5));
+const __VLS_7 = __VLS_pickFunctionalComponentCtx(__VLS_4, __VLS_6)!;
+{
+const __VLS_8 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_9 = __VLS_asFunctionalComponent(__VLS_8, {});
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_10 = __VLS_9({ ...{ ...{}, class: ("write"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_9));
+const __VLS_11 = __VLS_pickFunctionalComponentCtx(__VLS_8, __VLS_10)!;
+{
+const __VLS_12 = ({} as __VLS_IntrinsicElements)["button"];
+const __VLS_13 = __VLS_asFunctionalComponent(__VLS_12, {});
+({} as __VLS_IntrinsicElements).button;
+({} as __VLS_IntrinsicElements).button;
+const __VLS_14 = __VLS_13({ ...{ ...{}, }, }, ...__VLS_functionalComponentArgsRest(__VLS_13));
+const __VLS_15 = __VLS_pickFunctionalComponentCtx(__VLS_12, __VLS_14)!;
+(__VLS_15.slots!).default;
+}
+(__VLS_11.slots!).default;
+}
+{
+const __VLS_16 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_17 = __VLS_asFunctionalComponent(__VLS_16, {});
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_18 = __VLS_17({ ...{ ...{}, class: ("preview"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_17));
+const __VLS_19 = __VLS_pickFunctionalComponentCtx(__VLS_16, __VLS_18)!;
+{
+const __VLS_20 = ({} as __VLS_IntrinsicElements)["button"];
+const __VLS_21 = __VLS_asFunctionalComponent(__VLS_20, {});
+({} as __VLS_IntrinsicElements).button;
+({} as __VLS_IntrinsicElements).button;
+const __VLS_22 = __VLS_21({ ...{ ...{}, }, }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+(__VLS_23.slots!).default;
+}
+(__VLS_19.slots!).default;
+}
+(__VLS_7.slots!).default;
+}
+{
+const __VLS_24 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_25 = __VLS_asFunctionalComponent(__VLS_24, {});
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_26 = __VLS_25({ ...{ ...{}, class: ("main"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_25));
+const __VLS_27 = __VLS_pickFunctionalComponentCtx(__VLS_24, __VLS_26)!;
+{
+const __VLS_28 = ({} as __VLS_IntrinsicElements)["textarea"];
+const __VLS_29 = __VLS_asFunctionalComponent(__VLS_28, {});
+({} as __VLS_IntrinsicElements).textarea;
+({} as __VLS_IntrinsicElements).textarea;
+const __VLS_30 = __VLS_29({ ...{ ...{}, placeholder: ("Leave a comment"), class: ("google-font"), cols: ("90"), rows: ("20"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_29));
+const __VLS_31 = __VLS_pickFunctionalComponentCtx(__VLS_28, __VLS_30)!;
+}
+(__VLS_27.slots!).default;
+}
+{
+const __VLS_32 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_33 = __VLS_asFunctionalComponent(__VLS_32, {});
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_34 = __VLS_33({ ...{ ...{}, class: ("footer google-font"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_33));
+const __VLS_35 = __VLS_pickFunctionalComponentCtx(__VLS_32, __VLS_34)!;
+{
+const __VLS_36 = ({} as __VLS_IntrinsicElements)["a"];
+const __VLS_37 = __VLS_asFunctionalComponent(__VLS_36, {});
+({} as __VLS_IntrinsicElements).a;
+({} as __VLS_IntrinsicElements).a;
+const __VLS_38 = __VLS_37({ ...{ ...{}, href: ("#"), }, }, ...__VLS_functionalComponentArgsRest(__VLS_37));
+const __VLS_39 = __VLS_pickFunctionalComponentCtx(__VLS_36, __VLS_38)!;
+(__VLS_39.slots!).default;
+}
+(__VLS_35.slots!).default;
+}
+(__VLS_3.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+__VLS_styleScopedClasses["container"];
+__VLS_styleScopedClasses["header"];
+__VLS_styleScopedClasses["write"];
+__VLS_styleScopedClasses["preview"];
+__VLS_styleScopedClasses["main"];
+__VLS_styleScopedClasses["google-font"];
+__VLS_styleScopedClasses["footer"];
+__VLS_styleScopedClasses["google-font"];
+}
+var __VLS_slots!: {};
+return __VLS_slots;
+}
