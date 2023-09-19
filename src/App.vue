ae8168b772e80c7ec4b466ab7fd13ada669c19be
yang_zhuo <2431542525@qq.com>
2023-09-19 15:18:06 +0800

    feat(App.vue)：样式基本调整完毕，可以解析markdown并正常显示

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,53 @@
 <script setup>
 import { marked } from 'marked'
-import { ref } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
 
 const showWriteBox = ref(true)
 const showPreviewBox = ref(false)
+const writeBoxRef = ref(null)
+const markdownContent = ref('')
 
-function toggleWriteBox(){
+function toggleWriteBox() {
     showWriteBox.value = true
     showPreviewBox.value = false
+    nextTick(() => {
+        if (writeBoxRef.value) {
+            writeBoxRef.value.focus()
+        }
+    })
 }
 
-function togglePreviewBox(){
+function togglePreviewBox() {
     showWriteBox.value = false
     showPreviewBox.value = true
 }
 
+onMounted(() => {
+    if (showWriteBox.value) {
+        writeBoxRef.value.focus()
+    }
+})
 </script>
 
 <template>
     <div class="container">
         <div class="header">
             <div class="ll-box"></div>
-            <div class="left-box" :class="{ 'selected-box':showWriteBox,'unselected-box':showPreviewBox}">
-                <button class="left-button" @click="toggleWriteBox" :class="{ 'selected-font': showPreviewBox }">write</button>
+            <div class="left-box" :class="{ 'selected-box': showWriteBox, 'unselected-box': showPreviewBox }">
+                <button class="left-button" @click="toggleWriteBox"
+                    :class="{ 'selected-font': showPreviewBox }">write</button>
             </div>
-            <div class="right-box" :class="{ 'selected-box':showPreviewBox,'unselected-box':showWriteBox }">
-                <button class="right-button" @click="togglePreviewBox" :class="{ 'selected-font': showWriteBox }">preview</button>
+            <div class="right-box" :class="{ 'selected-box': showPreviewBox, 'unselected-box': showWriteBox }">
+                <button class="right-button" @click="togglePreviewBox"
+                    :class="{ 'selected-font': showWriteBox }">preview</button>
             </div>
             <div class="rr-box"></div>
         </div>
 
         <div class="main">
-            <textarea  v-if="showWriteBox" placeholder="Leave a comment" class="google-font write-box" cols="90" rows="20">write-box</textarea>
-            <div  v-if="showPreviewBox" class="preview-box">preview-box</div>
+            <textarea v-model="markdownContent" ref="writeBoxRef" v-if="showWriteBox" placeholder="Leave a comment" class="google-font write-box"
+                cols="90" rows="20"></textarea>
+            <div v-if="showPreviewBox" class="preview-box" v-html="marked(markdownContent)"></div>
         </div>
 
         <div class="footer google-font">Remember, contributions to this repository should follow our <a href="#">Github
@@ -64,45 +79,60 @@ function togglePreviewBox(){
     width: 100%;
     height: 50px;
     display: flex;
-    border-bottom: 1px solid #d0d7de;
     padding-bottom: 1rem;
 }
 
-.left-box{
+.left-box {
     flex: 20%;
     background-color: #ffffff;
 }
 
-.right-box{
+.right-box {
     flex: 20%;
     background-color: #ffffff;
 }
 
-.selected-font{
+.selected-font {
     color: #999999;
 }
 
-.selected-box{
+.selected-box {
     border: 1px solid #d0d7de;
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
     border-bottom: none;
 }
 
-.unselected-box{
+.unselected-box {
     border-bottom: 1px solid #d0d7de;
 }
 
-.preview-box{
+.write-box {
+    outline: none;
+    border: 1px solid #d0d7de;
+}
+
+.write-box:focus {
+    border: 3px solid #218bff;
+    border-radius: 6px;
+}
+
+.write-box:not(:focus) {
+    background-color: #f6f8fa;
+}
+
+.preview-box {
     width: 500px;
     height: 200px;
     background-color: #fff;
 }
 
-.ll-box{
-    flex:2%;
+.ll-box {
+    flex: 2%;
     border-bottom: 1px solid #d0d7de;
+
 }
+
 .rr-box {
     flex: 60%;
     border-bottom: 1px solid #d0d7de;
@@ -116,13 +146,12 @@ function togglePreviewBox(){
     font-family: Roboto, sans-serif;
 }
 
-button{
-    border:none;
+button {
+    border: none;
     background-color: transparent;
     outline: none;
 }
 
-button:hover{
+button:hover {
     color: black;
-}
-</style>
+}</style>
